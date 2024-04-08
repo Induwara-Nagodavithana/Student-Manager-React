afad04a1d9ebf05617a161306e6732320c3e5861
Induwara-Nagodavithana <induwara.nagodavithana@gmail.com>
2024-04-08 11:59:55 +0530

    add new changes

--- src/utils/http-common.js
+++ src/utils/http-common.js
@@ -3,31 +3,26 @@ import Message from "utils/messages";
 
 const instance = axios.create({
   // baseURL: "http://node-env.eba-pakmdcpw.us-east-2.elasticbeanstalk.com/",
-  // baseURL: "https://npark-fitness-backend.herokuapp.com/",
   baseURL: "http://localhost:8080/",
-  // baseURL: "http://192.168.1.24:3005",
   timeout: 30000,
   headers: {
     "Access-Control-Allow-Origin": "http://localhost:3000",
     "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,PATCH,OPTIONS",
     "Access-Control-Allow-Headers":
       "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
-    // Authorization: `Bearer ${token}`,
+
     "Content-type": "application/json",
   },
 });
 
 instance.interceptors.request.use(
   (config) => {
-    // Do something before request is sent
     const token = localStorage.getItem("token");
-    // console.log("token");
-    // console.log(token);
+
     config.headers.Authorization = `Bearer ${token}`;
     return config;
   },
   (error) => {
-    // Do something with request error
     console.log(error);
     return Promise.reject(error);
   }
@@ -35,21 +30,18 @@ instance.interceptors.request.use(
 
 instance.interceptors.response.use(
   (response) => {
-    // Any status code that lie within the range of 2xx cause this function to trigger
-    // Do something with response data
-    // console.log(response);
+
     console.log(response.status);
     return response;
   },
   (error) => {
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
-    // Do something with response error
+
     console.log(error);
     console.log(error.response);
     if (error.response !== undefined && error.response.status === 401) {
       console.log("error");
       if ("sign-in" != window.location.href.slice(-7)) {
-        // window.location = "/";
+        window.location = "/";
       }
     } else {
       let msg = "Cannot find the Server";
